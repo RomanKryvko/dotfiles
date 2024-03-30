9deb88e5ee67271e3ebb65ecf3cb5059f764064a
roman <roman.kr43@gmail.com>
2024-03-30 05:58:27 +0100

    update wallpaper script interface

--- .config/scripts/set-wallpaper.sh
+++ .config/scripts/set-wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-work_path=$HOME/Pictures/Wallpapers 
+#work_path=$HOME/Pictures/Wallpapers 
+work_path=$1
 files=($(ls $work_path))
 file_count=$(ls $work_path | wc -l) 
 number=$(($RANDOM % $file_count))
