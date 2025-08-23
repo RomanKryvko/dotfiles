18934225dcde76844474bf86e0613b5b28f8a305
Roman Kryvko <roman.kr43@gmail.com>
2025-08-23 11:51:27 +0200

    Update lastcore script.

--- scripts/dot-local/bin/scripts/lastcore.sh
+++ scripts/dot-local/bin/scripts/lastcore.sh
@@ -1,10 +1,15 @@
 #/bin/bash
 base_corepath="/var/lib/systemd/coredump/"
-corename=$(ls $base_corepath -t | head -n1)
+corename=$(ls $base_corepath -t | head -1)
 corepath="${base_corepath}${corename}"
+
+coredumpctl -1
 
 sudo cp $corepath $PWD
 sudo chown $USER $corename
-unzstd $corename
+unzstd $corename > /dev/null 2>&1
 
 rm $corename
+time_us=$(echo $corename | awk '{ split($0,a,"."); print a[6] }')
+core_time=$(date +"%Y-%m-%d_%H-%M-%S" -d @${time_us::-6})
+mv ${corename::-4} core_$core_time
