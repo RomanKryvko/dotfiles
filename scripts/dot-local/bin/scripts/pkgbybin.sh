ab798e8d8c5ee98f2c941b95ef21fb2e2029f79e
Roman Kryvko <roman.kr43@gmail.com>
2025-09-27 20:46:31 +0200

    Clean up the pkgbybin script.

--- scripts/dot-local/bin/scripts/pkgbybin.sh
+++ scripts/dot-local/bin/scripts/pkgbybin.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Prints package info by binary using pacman.
 if [ -z $1 ]; then echo "Usage: $(basename $0) [BINARY NAME]"; exit 1; fi
-binname=$(which $1)
-if [ $? -ne 0 ]; then echo $binname; exit 2; fi
-pkgname=$(pacman -Qoq $binname)
-if [ $? -ne 0 ]; then echo $pkgname; exit 3; fi
+binname=$(which $1) || exit 2
+pkgname=$(pacman -Qoq $binname) || exit 3
 pacman -Qi $pkgname
