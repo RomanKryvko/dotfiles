1ce12067a4b759bb0320a5a355d2e5fb1eebf947
Roman Kryvko <roman.kr43@gmail.com>
2026-01-06 16:15:33 +0100

    Set dictionary for spelling.

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
+vim.opt.dictionary = "spell"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
