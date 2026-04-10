8ca66f41269fb14dd85d8a420407022ebfe08ee4
Roman Kryvko <roman.kr43@gmail.com>
2026-04-11 01:11:26 +0200

    nvim: resolve colorscheme importing issue.

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -8,14 +8,4 @@ vim.g.have_nerd_font = true
 require("config.keymaps")
 require("config.autocommands")
 require("config.options")
-
--- NOTE: plugin directory should be sourced automatically, but it didn't work for me
-require("plugin.00-colorscheme")
-require("plugin.treesitter")
-require("plugin.misc")
-require("plugin.mini")
-require("plugin.completion")
-require("plugin.lsp")
-require("plugin.telescope")
-
 require("config.colorscheme")
