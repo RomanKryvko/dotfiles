a47a3da899237a6e6b9303148d40fdd58035a4bc
Roman Kryvko <roman.kr43@gmail.com>
2026-04-11 12:25:09 +0200

    nvim: fix treesitter config.
    Explicitly install required parsers. Remove unnecessary configs.

--- nvim/dot-config/nvim/plugin/treesitter.lua
+++ nvim/dot-config/nvim/plugin/treesitter.lua
@@ -1,17 +1,31 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
-require("nvim-treesitter").setup()
 -- Prefer git instead of curl in order to improve connectivity in some environments
 require("nvim-treesitter.install").prefer_git = true
-require("nvim-treesitter.config").setup({
-    ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
+
+local ensure_installed = {
+    "bash",
+    "c",
+    "cpp",
+    "css",
+    "html",
+    "javascript",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "rust",
+    "typescript",
+}
+local treesitter = require("nvim-treesitter")
+treesitter.install(ensure_installed)
+
+local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+        if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+            vim.treesitter.start(args.buf)
+        end
+    end,
 })
